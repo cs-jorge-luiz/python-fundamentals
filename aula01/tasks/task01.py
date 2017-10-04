e4f19526d171c1ab13d785ee38fd991a4c37daf3
Jorge Luiz Moreira <jorge.luiz@concrete.com.br>
2017-10-04 16:02:35 -0300

    Commit das issues referentes a milestone da Aula 01

---
+++ aula01/tasks/task01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+'''
+
+Task 01 - Identificando um usuario
+Fazer um script em python, que receba o login e senha
+de um determinado usuario, e mostre na tela:
+    Seja Bem Vindo [Usuario]
+Dica: Para fazer esse script voce precisara de duas
+variaveis e utilizar dos comandos raw_input e print.
+
+'''
+
+login = raw_input("Digite seu login: ")
+password = raw_input("Digite sua senha: ")
+
+print "Seja Bem Vindo %s"%login
---
+++ aula01/tasks/task02.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+'''
+
+Task 02 - Somente um usuario especificado pode logar
+
+Agora que voce ja fez um sistema de login basico, so sera possivel fazer a
+autenticacao de usuarios previamente definidos no nosso sistema.
+
+Para isso precisamos ter duas variaveis que vao armazenar um usuario e
+uma senha especificadas pelo programador.
+
+Por exemplo: usuario = arthur.dent senha = mochileiro
+
+Se o usuario informado for igual ao usuario especificado no sistema e a senha
+desse usuario for igual a senha especificada no sistema. Aparecera a mensagem:
+
+Usuario Autenticado com Sucesso!
+
+Seja Bem Vindo [Usuario]
+
+Caso contrario aparecera:
+
+Acesso Negado
+
+'''
+
+user = "arthur.dent"
+password = "mochileiro"
+
+login = raw_input("Digite seu login: ")
+senha = raw_input("Digite sua senha: ")
+
+if (login == user) and (senha == password):
+    print "Usuario Autenticado com sucesso! \nSeja Bem Vindo %s "%login
+else:
+    print "Acesso Negado!"
