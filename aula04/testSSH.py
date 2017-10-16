14607e340241dc859ef5b5ad4289df14f0493385
Jorge Luiz Moreira <jorge.luiz@concrete.com.br>
2017-10-16 13:54:22 -0200

    Commit continuacao Aula 04

--- aula04/testSSH.py
+++ aula04/testSSH.py
@@ -4,7 +4,13 @@ from paramiko.client import SSHClient
 import paramiko
 
 ssh = SSHClient()
+ssh.load_system_host_keys()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect("192.168.0.2")
+stdin,stdout,stderr = ssh.exec_command("ls -la")
+print stdout.read()
 
-ssh.client_load_system_host_keys()
-ssh.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-print ssh.connect("192.168.0.2")
+if stderr.channel.recv_exit_status != 0:
+    print stderr.read()
+else:
+    print stdout.read()
